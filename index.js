34670bbd02ffd7e3c259613477b6829dd16a205d
hasib9669 <hasibullah.rezaie@skyered-education.de>
2025-02-19 16:17:24 +0100

    change details

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Hier kannst du pro Skill einen Bildpfad hinterlegen,
 // z.B. 'linux': 'certificates/linux_cert.jpg'
 const skillImageMap = {
-  "linux & bash": "certificates/linux_bash_cert.jpg",
+  "linux & bash": "./certificates/Abzeichen.png",
   // Weitere Zuordnungen möglich ...
   // Skills ohne Eintrag erhalten "Zertifikat ist in progress"
 };
